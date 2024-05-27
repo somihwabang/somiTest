b862d1051161443f30374841a4f9c4d6c2b66d2d
minjcho <117441212+minjcho@users.noreply.github.com>
2024-05-27 21:30:22 +0900

    feat: db connection 0.1

--- js/start.js
+++ js/start.js
@@ -183,11 +183,11 @@ function addAnswer(answerText, qIdx, idx) {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ department: lastSelected }),
+                body: JSON.stringify({ department: nextKey })
             })
             .then(response => response.text())
             .then(data => console.log(data))
-            .catch(error => console.error('Error:', error));
+            .catch((error) => console.error('Error:', error));
 
             for (let i = 0; i < children.length; i++) {
                 children[i].style.display = 'none';
