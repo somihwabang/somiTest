8cd5bcaef70ff3b3a135adc36f7f7a290d30e558
minjcho <117441212+minjcho@users.noreply.github.com>
2024-05-28 12:24:47 +0900

    fix: result page visualization

--- js/result.js
+++ js/result.js
@@ -7,88 +7,88 @@ window.addEventListener('DOMContentLoaded', () => {
     const resultDescElement = document.querySelector('.resultDesc');
     const resultImgContainer = document.querySelector('#resultImg');
 
-    // Set result name
     if (resultNameElement && lastSelected) {
         resultNameElement.innerHTML = lastSelected;
     }
 
-    // Add result image
     let imageUrl = '';
     if (resultImgContainer && lastSelected) {
         const resultImage = document.createElement('img');
-        imageUrl = `../image/results/${lastSelected}.png`; // Path to result image
+        imageUrl = `../image/results/${lastSelected}.png`;
         resultImage.src = imageUrl;
         resultImage.alt = lastSelected;
-        resultImage.className = 'img-fluid'; // Add Bootstrap class for responsive image
-        resultImage.style.maxWidth = '90%'; // Ensure the image does not exceed the container width
-        resultImage.style.height = 'auto'; // Maintain aspect ratio
-        resultImage.style.display = 'block'; // Center the image
-        resultImage.style.margin = '0 auto'; // Center the image
+        resultImage.className = 'img-fluid';
+        resultImage.style.maxWidth = '60%';
+		resultImage.style.maxWidth = '600px';
+        resultImage.style.height = 'auto';
+        resultImage.style.display = 'block';
+        resultImage.style.margin = '0 auto';
         resultImgContainer.appendChild(resultImage);
     }
 
-    // Find the selected major's details from infoList
     const selectedMajorInfo = infoList.find(majorInfo => majorInfo.major === lastSelected);
 
-    // Add result description and subjects
     if (selectedMajorInfo && resultDescElement) {
         const resultContainer = document.createElement('div');
         resultContainer.className = 'result-container';
 
         const descHeading = document.createElement('h2');
         descHeading.innerText = `${selectedMajorInfo.major}는요`;
-        descHeading.style.whiteSpace = "nowrap"; // Prevent line break
+		descHeading.style.marginTop = '30px';
+        descHeading.style.whiteSpace = "nowrap";
         descHeading.style.display = "block";
-        descHeading.style.width = "100%"; // Adjust width as needed
-        descHeading.style.overflow = "hidden"; // Ensure text stays within the container
-        descHeading.style.fontFamily = 'Maplestory Light'; // Set font
-		// font bold
-		descHeading.style.fontWeight = 'bold';
+        descHeading.style.width = "100%";
+        descHeading.style.overflow = "hidden";
+        descHeading.style.fontFamily = 'Maplestory Light';
+        descHeading.style.fontWeight = 'bold';
+		descHeading.style.fontSize = '4em';
 
         resultContainer.appendChild(descHeading);
 
         const descParagraph = document.createElement('p');
+		descParagraph.style.marginTop = '30px';
         descParagraph.innerText = selectedMajorInfo.desc;
-        descParagraph.style.fontFamily = 'Maplestory Light'; // Set font
-		// font size
-		descParagraph.style.fontSize = '1.5em';
+        descParagraph.style.fontFamily = 'Maplestory Light';
+        // descParagraph.style.fontSize = '1.5em';
         resultContainer.appendChild(descParagraph);
 
         const learningHeading = document.createElement('h2');
+		learningHeading.style.marginTop = '50px';
+		learningHeading.style.marginBottom = '30px';
         learningHeading.innerText = '이런 학과도 어울려요!';
-        learningHeading.style.whiteSpace = "nowrap"; // Prevent line break
+        learningHeading.style.whiteSpace = "nowrap";
         learningHeading.style.display = "block";
-        learningHeading.style.width = "100%"; // Adjust width as needed
-        learningHeading.style.overflow = "hidden"; // Ensure text stays within the container
-        learningHeading.style.fontFamily = 'Maplestory Light'; // Set font
-		// font bold
-		learningHeading.style.fontWeight = 'bold';
+        learningHeading.style.width = "100%";
+        learningHeading.style.overflow = "hidden";
+        learningHeading.style.fontFamily = 'Maplestory Light';
+        learningHeading.style.fontWeight = 'bold';
+		learningHeading.style.fontSize = '4.5em';
+
 
         resultContainer.appendChild(learningHeading);
 
         const learningList = document.createElement('ul');
-        learningList.style.listStyle = 'none'; // Remove list bullets
+        learningList.style.listStyle = 'none';
 
         selectedMajorInfo.learning.forEach(subject => {
             const listItem = document.createElement('li');
             listItem.style.display = 'flex';
             listItem.style.flexWrap = 'wrap';
-            listItem.style.fontFamily = 'Maplestory Light'; // Set font
+            listItem.style.fontFamily = 'Maplestory Light';
 
             const nameElement = document.createElement('strong');
             nameElement.style.flex = '0 0 auto';
             nameElement.style.marginRight = '5px';
             nameElement.innerText = `${subject.name}:`;
-            nameElement.style.fontFamily = 'Maplestory Light'; // Set font
-			// font size
-			nameElement.style.fontSize = '0.8em';
+            nameElement.style.fontFamily = 'Maplestory Light';
+            // nameElement.style.fontSize = '0.8em';
 
             const descElement = document.createElement('span');
             descElement.style.flex = '1 1 auto';
             descElement.innerText = subject.desc;
-            descElement.style.fontFamily = 'Maplestory Light'; // Set font
-			// font size
-			descElement.style.fontSize = '0.8em';
+            descElement.style.fontFamily = 'Maplestory Light';
+			descElement.style.marginBottom = '15px';
+            // descElement.style.fontSize = '0.8em';
 
             listItem.appendChild(nameElement);
             listItem.appendChild(descElement);
@@ -99,25 +99,23 @@ window.addEventListener('DOMContentLoaded', () => {
         resultDescElement.appendChild(resultContainer);
     }
 
-    // Add retry button and download button in a flex container
     if (resultDescElement) {
         const buttonContainer = document.createElement('div');
         buttonContainer.className = 'button-container';
 
         const retryButton = document.createElement('button');
         retryButton.innerHTML = '다시하기';
-        retryButton.className = 'btn retry-button'; // Reuse existing styles
+        retryButton.className = 'btn retry-button';
         retryButton.addEventListener('click', () => {
-            window.location.href = 'index.html'; // Navigate to index.html to retry
+            window.location.href = 'index.html';
         });
         buttonContainer.appendChild(retryButton);
 
-        // Add download button
         if (imageUrl) {
             const downloadButton = document.createElement('a');
-            downloadButton.href = `800w/${lastSelected}.png`; // Updated path for download
-            downloadButton.download = `소미화방_${lastSelected}.jpg`; // Custom file name
-            downloadButton.className = 'btn download-button'; // Reuse existing styles
+            downloadButton.href = `800w/${lastSelected}.png`;
+            downloadButton.download = `소미화방_${lastSelected}.jpg`;
+            downloadButton.className = 'btn download-button';
             downloadButton.innerHTML = '이미지 다운로드';
             buttonContainer.appendChild(downloadButton);
         }
@@ -125,21 +123,7 @@ window.addEventListener('DOMContentLoaded', () => {
         resultDescElement.appendChild(buttonContainer);
     }
 
-    // Adjust iframe height on window resize
     window.addEventListener('resize', () => {
         resultForm.height = window.innerHeight * 0.8 + 'px';
     });
-
-    // // Add Google Forms iframe
-    // const resultForm = document.createElement('iframe');
-    // resultForm.src = 'https://docs.google.com/forms/d/e/1FAIpQLSe37Lxv2IXRRp5XsT5geG5JS140kL8nFUnl1Kd3eaLX2BvwsA/viewform?embedded=true';
-    // resultForm.width = '100%';
-    // resultForm.height = window.innerHeight * 0.8 + 'px'; // Height set to 80% of the window height
-    // resultForm.frameBorder = '0';
-    // resultForm.marginHeight = '0';
-    // resultForm.marginWidth = '0';
-    // resultForm.style.border = 'none';
-    // resultForm.style.display = 'block';
-    // resultForm.style.margin = '20px auto'; // Add margin to top and bottom
-    // resultDescElement.appendChild(resultForm);
 });
